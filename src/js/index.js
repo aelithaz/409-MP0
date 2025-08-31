6677d52794e5cdb24816df506614a3e658df7198
aelithaz <anzhang@umass.edu>
2025-08-31 16:12:09 -0500

    Initial framework
    - Created basic bio card and styling
    - Added three color changing buttons
    - Replaced default picture with a selfie

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-/* Your JS here. */
-console.log('Hello World!')
+import '../css/main.css';
+
+function changeBackground(color) {
+  document.querySelector('.card').style.backgroundColor = color;
+}
+
+// Make it accessible from inline HTML `onclick` handlers
+window.changeBackground = changeBackground;
